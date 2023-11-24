0c8eea7249468bba376efe4340689abf37724436
Manos <igoufe.osman@gmail.com>
2023-11-24 17:56:11 +0100

    Modifications des fichier event-bus et client : Ajout d'un control d'erreur détaillé à l'affichage

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,41 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import axios from "axios";
 import CommentCreate from "./CommentCreate";
 import CommentList from "./CommentList";
 
 const PostList = () => {
-  const [posts, setPosts] = useState({});
+    const [posts, setPosts] = useState({});
+    const [error, setError] = useState(null);
 
-  const fetchPosts = async () => {
-    const res = await axios.get("http://localhost/posts");
+    const fetchPosts = async () => {
+        try {
+            const res = await axios.get("http://localhost/posts");
+            setPosts(res.data);
+        } catch (error) {
+            console.error('Erreur Axios lors de la récupération des posts:', error.message);
+            setError('Une erreur s\'est produite lors de la récupération des posts.');
+        }
+    };
 
-    setPosts(res.data);
-  };
+    useEffect(() => {
+        fetchPosts();
+    }, []);
 
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+    const renderedPosts = Object.values(posts).map((post) => {
+        return (
+            <div
+                className="card"
+                style={{width: "30%", marginBottom: "20px"}}
+                key={post.id}
+            >
+                <div className="card-body">
+                    <h3>{post.title}</h3>
+                    <CommentList comments={post.comments}/>
+                    <CommentCreate postId={post.id}/>
+                </div>
+            </div>
+        );
+    });
 
-  const renderedPosts = Object.values(posts).map((post) => {
     return (
-      <div
-        className="card"
-        style={{ width: "30%", marginBottom: "20px" }}
-        key={post.id}
-      >
-        <div className="card-body">
-          <h3>{post.title}</h3>
-          <CommentList comments={post.comments} />
-          <CommentCreate postId={post.id} />
+        <div>
+            {error && <div className="alert alert-danger">{error}</div>}
+            <div className="d-flex flex-row flex-wrap justify-content-between">
+                {renderedPosts}
+            </div>
         </div>
-      </div>
     );
-  });
-
-  return (
-    <div className="d-flex flex-row flex-wrap justify-content-between">
-      {renderedPosts}
-    </div>
-  );
 };
+
 export default PostList;
--- event-bus/index.js
+++ event-bus/index.js
@@ -7,30 +7,28 @@ app.use(bodyParser.json());
 
 const events = [];
 
-app.post("/events", (req, res) => {
-  const event = req.body;
+app.post("/events", async (req, res) => {
+    try {
+        const event = req.body;
 
-  events.push(event);
+        events.push(event);
 
-  axios.post("http://posts-clusterip-srv:4000/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://comments-srv:4001/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://query-srv:4002/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  axios.post("http://moderation-srv:4003/events", event).catch((err) => {
-    console.log(err.message);
-  });
-  res.send({ status: "OK" });
+        await axios.post("http://posts-clusterip-srv:4000/events", event);
+        await axios.post("http://comments-srv:4001/events", event);
+        await axios.post("http://query-srv:4002/events", event);
+        await axios.post("http://moderation-srv:4003/events", event);
+
+        res.send({status: "OK"});
+    } catch (error) {
+        console.error("Erreur lors de la gestion des événements:", error.message);
+        res.status(500).send({error: "Erreur lors de la gestion des événements"});
+    }
 });
 
 app.get("/events", (req, res) => {
-  res.send(events);
+    res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log("Listening on 4005");
+    console.log("Listening on 4005");
 });
