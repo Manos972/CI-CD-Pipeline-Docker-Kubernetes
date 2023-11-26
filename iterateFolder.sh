166320ffecafc2b39259b6e55a186979eceee246
Manos <igoufe.osman@gmail.com>
2023-11-26 11:24:36 +0100

    Utilisation d'un fichier de script pour l'itération du repertoire pour exclure le dossier de gestion configuration Kubernetes ./infra/k8s

---
+++ iterateFolder.sh
@@ -0,0 +1,8 @@
+for dir in */; do
+  dir=${dir%*/}
+  if [ "$dir" != "infra" ]; then
+    image_name="$CI_REGISTRY/manos972/tp_fil_rouge/${dir}:v1.3"
+    docker build -t $image_name ./$dir
+    docker push $image_name
+  fi
+done
