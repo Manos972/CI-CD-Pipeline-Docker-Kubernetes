85e58c915315ecef45439393f09809ef49facb62
Manos <igoufe.osman@gmail.com>
2023-11-26 22:11:20 +0100

    Modification de la version de l'image v1.4

--- iterateFolder.sh
+++ iterateFolder.sh
@@ -1,7 +1,7 @@
 for dir in */; do
   dir=${dir%*/}
   if [ "$dir" != "infra" ]; then
-    image_name="$CI_REGISTRY/manos972/tp_fil_rouge/${dir}:v1.3"
+    image_name="$CI_REGISTRY/manos972/tp_fil_rouge/${dir}:v1.4"
     docker build -t $image_name ./$dir
     docker push $image_name
   fi
